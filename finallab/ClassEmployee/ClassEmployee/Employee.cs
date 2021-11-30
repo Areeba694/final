8f7e9cb8afb7b95b7a9e132a6baf871248aba598
Areeba694 <91488602+Areeba694@users.noreply.github.com>
2021-11-30 12:22:07 +0500

    final lab folder

---
+++ finallab/ClassEmployee/ClassEmployee/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassEmployee
+{
+    public abstract class Employee
+    {
+        public int basePay;
+        public int base_hours;
+        public int hourlyPayRate;
+        public int no_of_units;
+        public int overtimehours;
+        public int overtimePay;
+        public int unitPay;
+    }
+
+    abstract void Calculate_Salary();
+}
+        
+   
---
+++ finallab/ClassEmployee/ClassEmployee/HourlySalary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassEmployee
+{
+    public class HourlySalary
+    {
+        public void Calculate_Salary()
+        {
+            if (Hours_worked > base_hours) ;
+            {
+                basePay = Hours_worked * base_hours;
+                overtimehours = Hours_worked - base_hours;
+                overtimePay = overtimehours * hourlyPayRate * OVERTIME_RATE;
+                unitPay = 3 * no_of_units;
+                GrossPay = basePay + overtimePay + unitPay;
+            }
+        }
+    }
+}
